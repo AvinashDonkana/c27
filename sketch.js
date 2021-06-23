5431c188424dd81063b01fd33a00d8afad8d1f74
AvinashDonkana <avinashdonkana2010@gmail.com>
2021-06-23 15:47:43 +0530

    test

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var constrainedLog;
+var chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -33,8 +37,11 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    constrainedLog=new Log(230,180,80,PI/2);
 
     bird = new Bird(100,100);
+
+    chain=new Chain(bird.body,constrainedLog.body);   
 
 }
 
@@ -61,4 +68,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    constrainedLog.display();
+    chain.display();
 }
